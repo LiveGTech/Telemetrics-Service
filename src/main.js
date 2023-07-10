ce5dd39c44dad54bf63cb9b36a66d4b89d2c4fa4
James-Livesey <jamesliveg@gmail.com>
2023-07-10 18:19:54 -0400

    Add basic API

---
+++ src/main.js
@@ -0,0 +1,34 @@
+/*
+    Telemetrics Service
+
+    Copyright (C) LiveG. All Rights Reserved.
+
+    https://liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+const package = require("../package.json");
+const express = require("express");
+
+var app = express();
+
+app.get("/", function(request, response) {
+    response.send({
+        status: "ok",
+        version: package.version
+    });
+});
+
+app.use(function(request, response, next) {
+    response.status(404);
+
+    response.send({
+        status: "error",
+        code: "invalidEndpoint",
+        message: "The endpoint requested is invalid."
+    });
+});
+
+app.listen(process.argv[2], function() {
+    console.log(`LiveG Telemetrics started on port ${process.argv[2]}`);
+});
